7add19c09f8db1ee7b47a0ef6c2f3058bae30fcf
tamon kondo <tamon.kondo@japan-coding.co.jp>
2026-05-22 10:48:19 +0900

    useSWR導入

--- apps/notion-training-app/src/App.tsx
+++ apps/notion-training-app/src/App.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 
 import { ExamplePage } from "./features/example/pages/ExamplePage";
+import HomePage from "./pages/HomePage";
 
 export function App() {
   return (
     <Routes>
-      <Route path="/" element={<ExamplePage />} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   );
 }
--- apps/notion-training-app/src/pages/HomePage.tsx
+++ apps/notion-training-app/src/pages/HomePage.tsx
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
+import useSWR from "swr";
+import fetcher from "../lib/fetch";
 
 const HomePage = () => {
+  const { data } = useSWR(
+    `${import.meta.env.VITE_API_URL}/training-logs`,
+    fetcher,
+  );
   useEffect(() => {
     // Your effect logic here
   }, []);
 
-  return <div>HomePage</div>;
+  return <div>{data ? JSON.stringify(data) : "Loading..."}</div>;
 };
 
 export default HomePage;
