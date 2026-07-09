d60b36950f0eb4da093d4d5b70b92aafc2caea61
tamon kondo <kondo0130thevamps@gmail.com>
2026-07-09 19:55:22 +0900

    パッケージバージョンアップデート

--- packages/schemas/notion-training-app/index.ts
+++ packages/schemas/notion-training-app/index.ts
@@ -1,3 +1,27 @@
+import { z } from "zod";
+import {
+  EXERCISE_GUIDE_LINE_REPS,
+  EXERCISE_TREND_PERIODS,
+} from "@repo/types/notion-training-app";
+
+const exercisesSets = z.object({
+  kg: z.string(),
+  rep: z.string(),
+  memo: z.string(),
+});
+const exercisesSchema = z.object({
+  exerciseId: z.string(),
+  rest: z.number(),
+  memo: z.string(),
+  sets: z.array(exercisesSets),
+});
+
+export const createTrainingLogSchema = z.object({
+  date: z.string(),
+  bodyWeight: z.number(),
+  memo: z.string(),
+  exercises: z.array(exercisesSchema),
+});
 
 export type CreateTrainingLogInput = z.infer<typeof createTrainingLogSchema>;
 
